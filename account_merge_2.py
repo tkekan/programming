72dc0240d058f14626232969b65ed1340d9e4ed3
Tanvir Kekan <tanvir.kekan@gmail.com>
2019-07-22 11:43:15 -0700

    corrected account_merge_2.py

--- account_merge_2.py
+++ account_merge_2.py
@@ -1,13 +1,20 @@
 from collections import defaultdict
 class Solution(object):
     
-    def dfs(self, temp, values, visited, accounts):
+    def dfs(self, temp, values, visited, accounts, dic):
         idx = values.pop(0)
+        if visited[idx] == True:
+            return
         visited[idx] = True
+        idx_set = set()
         for emails in accounts[idx][1:]:
             temp.add(emails)
-        if len(values) and visited[values[0]] == False:
-            self.dfs(temp, values, visited, accounts)
+            for ids in dic[emails]:
+                if visited[ids] == False:
+                    idx_set.add(ids)
+        [values.append(x) for x in idx_set]
+        while len(values): 
+            self.dfs(temp, values, visited, accounts, dic)
         
     def accountsMerge(self, accounts):
         """
@@ -26,13 +33,17 @@ class Solution(object):
             temp.add(keys)
             name = accounts[values[0]][0]
             if visited[values[0]] == False:
-                self.dfs(temp,values, visited, accounts)
-                results.append([name] +list(temp))
+                self.dfs(temp,values, visited, accounts, dic)
+                results.append([name] +sorted(list(temp)))
                     
         print results
 
 
 s = Solution()
+#PASS
 accounts = [["John","johnsmith@mail.com","john_newyork@mail.com"],["John","johnsmith@mail.com","john00@mail.com"],["Mary","mary@mail.com"],["John","johnnybravo@mail.com"]]
+#PAss
 accounts = [["Alex","Alex5@m.co","Alex4@m.co","Alex0@m.co"],["Ethan","Ethan3@m.co","Ethan3@m.co","Ethan0@m.co"],["Kevin","Kevin4@m.co","Kevin2@m.co","Kevin2@m.co"],["Gabe","Gabe0@m.co","Gabe3@m.co","Gabe2@m.co"],["Gabe","Gabe3@m.co","Gabe4@m.co","Gabe2@m.co"]]
+#FAIL
+accounts = [["David","David0@m.co","David4@m.co","David3@m.co"],["David","David5@m.co","David5@m.co","David0@m.co"],["David","David1@m.co","David4@m.co","David0@m.co"],["David","David0@m.co","David1@m.co","David3@m.co"],["David","David4@m.co","David1@m.co","David3@m.co"]]
 s.accountsMerge(accounts)
