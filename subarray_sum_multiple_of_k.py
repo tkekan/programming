8dfcbeae4a828bc5e04360216a842d294840c747
Tanvir Kekan <tanvir.kekan@gmail.com>
2019-07-22 01:03:27 -0700

    subarray

---
+++ subarray_sum_multiple_of_k.py
@@ -0,0 +1,15 @@
+d = dict()
+d[0] = -1
+sums = 0
+
+for i in range(len(nums)):
+	sums+=nums[i]
+	if(k!=0):
+		sums = sums%k
+	if(sums in d):
+		if(i-d[sums]>1):
+			return(True)
+	else:
+		d[sums] = i
+
+return(False)
