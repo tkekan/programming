f44d19b9719b7f70064c895c255567c214f0addb
Tanvir Kekan <tanvir.kekan@gmail.com>
2019-07-25 14:46:19 -0700

    Infix expression evaluation

---
+++ calculator.py
@@ -0,0 +1,62 @@
+
+def add(a,b):
+    return int(a)+int(b)
+    
+def sub(a,b):
+    return int(a)-int(b)
+    
+def mult(a,b):
+    return int(a) * init(b)
+
+def div(a,b):
+    try:
+        return float(int(a) / int(b))
+    except Exception as e:
+        print e
+    
+    
+function_map = {'+' : add,
+                '-' : sub, '*' : mult, '/' :div }
+
+def precedence(op):
+    if op in ['+', '-']:
+        return 1
+    elif op in ['*', '/']:
+        return 2
+
+def calculator_input(data):
+    data = list(data)
+    data_s = []
+    op_s = []
+    result = 0
+    operator = None
+    data_str = ''
+    for index in range(0,len(data)):
+        if data[index].isalnum():
+            data_str += data[index]
+            try:
+                if not data[index+1].isalnum():
+                    data_s.append(data_str) 
+                    data_str = ''
+            except Exception as e:
+                if len(data_str):
+                   data_s.append(data_str)
+        else:
+            if len(op_s):
+                while precedence(op_s[-1]) > precedence(data[index]):
+                    num2 = data_s.pop()
+                    num1 = data_s.pop()
+                    value = function_map[op_s.pop()](num1,num2)
+                    data_s.push(value)
+            op_s.append(data[index])
+
+    while len(op_s):
+        num2 = data_s.pop()
+        num1 = data_s.pop()
+        value = function_map[op_s.pop()](num1,num2)
+        data_s.append(value)
+    print data_s[-1]
+                    
+
+input_s = "43+4/2"         
+calculator_input(input_s)
